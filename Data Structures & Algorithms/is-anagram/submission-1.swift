16f2f75aa9ffddd6ea0d08a89d5f30ae2b258d00
rishabhb5 <38149974+rishabhb5@users.noreply.github.com>
2026-05-07 10:25:36 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.swift
@@ -0,0 +1,37 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        /* iterate through each string once
+        When iterating through String s, create a hashmap and keep count of char and
+        count. When iterating through String t check hashmap and decrement count of
+        each char seen. Then check if the Hashmap is empty*/
+        // O(n) time | O (n) space
+
+        // in Swift,  Hashmap is called a Dictionary
+        var hashmap = [Character: Int]()
+        let sArr = Array(s)
+        let tArr = Array(t)
+
+        for i in 0..<sArr.count {
+            hashmap[sArr[i], default: 0] += 1 
+        }
+
+        for j in 0..<tArr.count {
+            hashmap[tArr[j], default: 0] -= 1
+
+            if hashmap[tArr[j]] == 0 {
+                hashmap.removeValue(forKey: tArr[j])
+            }
+        }
+
+        if hashmap.isEmpty {
+            return true
+        }
+        else {
+            return false
+        }
+
+        
+
+
+    }
+}
