519ed5c35a46abd0a8f56c4eaa7fb815ed9805ec
rishabhb5 <38149974+rishabhb5@users.noreply.github.com>
2026-07-14 20:32:06 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+# O(n^2) Time
+# O(1) Space
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {} # val : index
+
+        for i in range(len(nums)):
+            hm[nums[i]] = i
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hm and hm.get(diff) != i:
+                return [i, hm.get(diff)]
+        return []
